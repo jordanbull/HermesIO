a5b50b891de5eafd6546dd5235c3feacac318620
jordangbull <jordangbull@gmail.com>
2014-04-08 19:53:18 -0700

    [Android] gui shows display state. also set up communication between service and activity

--- android/src/com/jbull/hermes/android/HermesActivity.java
+++ android/src/com/jbull/hermes/android/HermesActivity.java
@@ -1,12 +1,23 @@
 package com.jbull.hermes.android;
 
 import android.app.Activity;
-import android.content.Intent;
+import android.content.*;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import org.jbull.jmessage.R;
 
 public class HermesActivity extends Activity {
+    private final String CONNECTED = "Connected";
+    private final String DISCONNECTED = "Disconnected";
+    HermesService mService;
+    boolean mBound = false;
+    TextView connectionStatusLabel;
+    Button connectButton;
+
     /**
      * Called when the activity is first created.
      */
@@ -14,7 +25,20 @@ public class HermesActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        connectionStatusLabel = (TextView) findViewById(R.id.connectionStatusLabel);
+        connectButton = (Button) findViewById(R.id.connectButton);
+    }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        registerReceiver(onBroadcast, new IntentFilter("com.jbull.hermes"));
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        unregisterReceiver(onBroadcast);
     }
 
     public void connectAndSetup(View view) {
@@ -22,13 +46,49 @@ public class HermesActivity extends Activity {
         //final String ip = ((EditText) findViewById(R.id.ipConnectField)).getText().toString();
         final String ip = "192.168.1.129";
         intent.putExtra("ip", ip);
+        bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
         new Thread(new Runnable() {
             @Override
             public void run() {
                     startService(intent);
             }
         }).start();
-
-
     }
+
+    public void setConnected(boolean connected) {
+        if (connected) {
+            connectButton.setEnabled(false);
+            connectionStatusLabel.setText(CONNECTED);
+        } else {
+            connectButton.setEnabled(true);
+            connectionStatusLabel.setText(DISCONNECTED);
+        }
+    }
+
+    /** Defines callbacks for service binding, passed to bindService() */
+    private ServiceConnection mConnection = new ServiceConnection() {
+
+        @Override
+        public void onServiceConnected(ComponentName className,
+                                       IBinder service) {
+            // We've bound to LocalService, cast the IBinder and get LocalService instance
+            HermesService.LocalBinder binder = (HermesService.LocalBinder) service;
+            mService = binder.getService();
+            mBound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName arg0) {
+            mBound = false;
+        }
+    };
+
+    private BroadcastReceiver onBroadcast = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context ctxt, Intent i) {
+            // do stuff to the UI
+            Log.w("Hermes", i.getAction());
+            setConnected(i.getBooleanExtra("connected", false));
+        }
+    };
 }
--- android/src/com/jbull/hermes/android/HermesService.java
+++ android/src/com/jbull/hermes/android/HermesService.java
@@ -3,6 +3,7 @@ package com.jbull.hermes.android;
 import android.app.Service;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.os.Binder;
 import android.os.IBinder;
 import android.util.Log;
 import com.jbull.hermes.*;
@@ -19,6 +20,8 @@ public class HermesService extends Service {
     private final int TIMEOUT_MILLIS = 5000;
     private Intent intent;
     private SendFavoredCommunicationScheduler commManager;
+
+    private final IBinder mBinder = new LocalBinder();
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
@@ -37,6 +40,9 @@ public class HermesService extends Service {
         MessageSender sender = new MessageSender(connection, numRetries);
         commManager = new SendFavoredCommunicationScheduler(sender, listener, new Runnable() {public void run() {disconnect();}}, SEND_PERIOD);
         handler.setCommunicationScheduler(commManager);
+        Intent connectedIntent = new Intent("com.jbull.hermes");
+        connectedIntent.putExtra("connected", true);
+        getApplicationContext().sendBroadcast(connectedIntent);
         Log.w("jMessage", "sending setup");
         new Thread(new Runnable() {
             @Override
@@ -54,9 +60,19 @@ public class HermesService extends Service {
     public void disconnect() {
         Log.w("HermesIO", "Disconnected");
         commManager.stop();
+        Intent connectedIntent = new Intent("com.jbull.hermes");
+        connectedIntent.putExtra("connected", false);
+        getApplicationContext().sendBroadcast(connectedIntent);
     }
 
     public IBinder onBind(Intent intent) {
-        return null;
+        return mBinder;
+    }
+
+    public class LocalBinder extends Binder {
+        HermesService getService() {
+            // Return this instance of LocalService so clients can call public methods
+            return HermesService.this;
+        }
     }
 }
