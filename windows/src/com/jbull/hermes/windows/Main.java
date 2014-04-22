98295dfc31646a097dd15ed3ba7fe5b243b1153d
Jordan Bull <jordangbull@gmail.com>
2014-04-22 00:00:52 -0700

    [Windows] Create Windows launch

---
+++ windows/src/com/jbull/hermes/windows/Main.java
@@ -0,0 +1,43 @@
+package com.jbull.hermes.windows;
+
+import com.jbull.hermes.desktop.CommunicationCenter;
+import com.jbull.hermes.desktop.DefaultNotification;
+import com.jbull.hermes.desktop.State;
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+
+import java.io.IOException;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        long start = System.currentTimeMillis();
+        final CommunicationCenter communicationCenter = new CommunicationCenter(new DefaultNotification(), new DefaultNotification());
+        final State state = new State(communicationCenter);
+        System.out.println(System.currentTimeMillis() - start);
+        primaryStage.setTitle("HermesIO");
+        primaryStage.setScene(new Scene(communicationCenter, 800, 600));
+        primaryStage.setMinWidth(communicationCenter.getPrefWidth());
+        primaryStage.setMinHeight(communicationCenter.getPrefHeight());
+        primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            public void handle(WindowEvent we) {
+                try {
+                    state.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                System.exit(0);
+            }
+        });
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
